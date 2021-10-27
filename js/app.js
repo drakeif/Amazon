194ac3e284fe27aa3d5bdb96871526c754e5c5c7
volodymyr <v.ohlopkov100@gmail.com>
2021-10-27 22:27:24 +0300

    v 0.4
    + categories

--- js/app.js
+++ js/app.js
@@ -19,3 +19,26 @@ var swiper = new Swiper(".mySwiper", {
       type: "progressbar",
     },
   });
+// SWIPER 2
+var swiper = new Swiper(".mySwiperCategories", {
+  slidesPerView: 3,
+  spaceBetween: 5,
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 3,
+      spaceBetween: 5,
+    },
+    "@0.75": {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+    "@1.00": {
+      slidesPerView: 6,
+      spaceBetween: 20,
+    },
+    "@1.50": {
+      slidesPerView: 8,
+      spaceBetween: 30,
+    },
+  },
+});
